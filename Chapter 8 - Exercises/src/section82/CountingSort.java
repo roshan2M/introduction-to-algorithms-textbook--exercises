6dcee90eb380cc1b194d3cf7d730a4df0a0a4f10
Roshan Munjal <roshan.munjal@hotmail.com>
2018-05-15 08:02:13 -0400

    Fixed counting sort

--- Chapter 8 - Exercises/src/section82/CountingSort.java
+++ Chapter 8 - Exercises/src/section82/CountingSort.java
@@ -8,16 +8,23 @@ public class CountingSort {
 		int[] a = {8, 0, 1, 4, 3, 7, 9, 6, 2, 5};
 		int[] b = countingSort(a, 9);
 		System.out.println(Arrays.toString(b));
+		
+		int[] c = {2, 16, 4, 4, 5, 1, 0, 3, 8, 12};
+		int[] d = countingSort(c, 16);
+		System.out.println(Arrays.toString(d));
 	}
 	
 	public static int[] countingSort(int[] a, int k) {
-		int[] b = new int[a.length];
-		int[] c = new int[k];
-		for (int i = 0; i < a.length; i++) c[a[i]]++;
-		for (int i = 1; i < k; i++) c[i] += c[i - 1];
-		for (int i = a.length - 1; i > 0; i--) {
-			b[c[a[i]] - 1] = a[i];
-			c[a[i]]--;
+		int n = a.length;
+		int[] b = new int[n];
+		int[] count = new int[k + 1];
+		
+		for (int i = 0; i <= k; i++) count[i] = 0;
+		for (int i = 0; i < n; i++) count[a[i]]++;
+		for (int i = 1; i <= k; i++) count[i] += count[i - 1];
+		for (int i = 0; i < n; i++) {
+			b[count[a[i]]-1] = a[i];
+			count[a[i]]--;
 		}
 		return b;
 	}
